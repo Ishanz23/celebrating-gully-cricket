f51aa6fbeeb1c3235245937e9013b35f669264f8
Sayantan Dey <sayantandey23@gmail.com>
2019-11-19 02:04:52 +0530

    multiple tournaments added to view scroll event

--- src/app/components/tournament-panel/tournament-panel.component.ts
+++ src/app/components/tournament-panel/tournament-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tournament-panel',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournament-panel.component.scss']
 })
 export class TournamentPanelComponent implements OnInit {
+  @Input() tournamentName: string;
+  @Input() season: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
