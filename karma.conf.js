82d2edb583615e49fcf8146b1ae2d14a84d90714
Sayantan Dey <sayantand@erevmax.grp>
2019-11-18 21:21:22 +0530

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/celebrating-para-cricket'),
+      dir: require('path').join(__dirname, './coverage/celebrating-gully-cricket'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'celebrating-para-cricket'`, () => {
+  it(`should have as title 'celebrating-gully-cricket'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('celebrating-para-cricket');
+    expect(app.title).toEqual('celebrating-gully-cricket');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('celebrating-para-cricket app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('celebrating-gully-cricket app is running!');
   });
 });
