c91fc3ccd2345d66330dab9f6bb572e54496553e
Sayantan Dey <sayantand@erevmax.grp>
2019-11-19 19:39:46 +0530

    initial loader animation fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { environment } from '../environments/environment'
 
-import { MatSelectModule } from '@angular/material/select';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreRouterConnectingModule } from '@ngrx/router-store'
+import { StoreModule } from '@ngrx/store'
 
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
+import { MatSelectModule } from '@angular/material/select'
 
-import { reducers, metaReducers } from './reducers';
-import { AppEffects } from './app.effects';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { TournamentPanelComponent } from './components/tournament-panel/tournament-panel.component';
+import { reducers, metaReducers } from './reducers'
+import { AppEffects } from './app.effects'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { HomeComponent } from './components/home/home.component'
+import { TournamentPanelComponent } from './components/tournament-panel/tournament-panel.component'
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent, TournamentPanelComponent],
