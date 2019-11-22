7344c41c3067a3e84556b69503845c42555154f9
Sayantan Dey <sayantand@erevmax.grp>
2019-11-22 21:32:54 +0530

    (design) css updates

--- src/app/components/tournament-panel/tournament-panel.component.ts
+++ src/app/components/tournament-panel/tournament-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Router } from '@angular/router'
+import { MatSnackBar, MatSnackBarVerticalPosition, MatSnackBarHorizontalPosition } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-tournament-panel',
@@ -9,11 +10,31 @@ import { Router } from '@angular/router'
 export class TournamentPanelComponent implements OnInit {
   @Input() tournament: any
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private _snackBar: MatSnackBar) {}
 
   ngOnInit() {}
 
-  navigateToEnrollment() {
-    this.router.navigate(['enroll', this.tournament.id])
+  navigate(event: Event, path: string) {
+    this.router.navigate([path, this.tournament.id])
+  }
+
+  showComingSoon() {
+    this.openSnackBar('Feature Coming Soon!')
+  }
+
+  private openSnackBar(
+    message: string,
+    action: string = '',
+    duration: number = 2000,
+    panelClass = 'accent',
+    verticalPosition: MatSnackBarVerticalPosition = 'bottom',
+    horizontalPosition: MatSnackBarHorizontalPosition = 'center'
+  ) {
+    this._snackBar.open(message, action, {
+      duration,
+      panelClass,
+      verticalPosition,
+      horizontalPosition
+    })
   }
 }
